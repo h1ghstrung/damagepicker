3151510fb35fb096dd859e3e3dc964971ef61191
h1ghstrung <pydev4hire@gmail.com>
2023-08-15 18:09:04 -0400

    Initial Commit

---
+++ notes.js
@@ -0,0 +1,11 @@
+/*
+Project: DnD Table Picker
+Author: h1ghstrung
+Date: 08/13/23
+*/
+
+/*
+This is a simple randomizer that will select an item from a
+gaming table and return the item. You may also select an item
+directly or enter your physical dice roll.
+*/
